1d129188bd81d2ef2e802c9524f33b71bc2562ba
orzechdev <tarasiukdawid@gmail.com>
2019-02-25 19:48:06 +0100

    Fix webview too small view

--- lib/widgets/schedule_webview.dart
+++ lib/widgets/schedule_webview.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappbrowser/flutter_inappbrowser.dart';
 
-const kAndroidUserAgent =
-    'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36';
-
 String selectedUrl = 'http://zschocianow.pl/plan/lista.html';
 
 class ScheduleWebview extends StatefulWidget {
@@ -18,7 +15,7 @@ class ScheduleWebviewState extends State<ScheduleWebview> {
       child: InAppWebView(
         initialUrl: selectedUrl,
         initialOptions: {
-          'userAgent': kAndroidUserAgent, // TODO Resizing doesn't work
+          'useWideViewPort': false,
         },
       ),
     );
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,12 +5,12 @@
 // are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+//import 'package:flutter_test/flutter_test.dart';
 
 import 'package:replacements/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+//  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
 //    await tester.pumpWidget(new MyApp());
 //
@@ -25,5 +25,5 @@ void main() {
 //    // Verify that our counter has incremented.
 //    expect(find.text('0'), findsNothing);
 //    expect(find.text('1'), findsOneWidget);
-  });
+//  });
 }
